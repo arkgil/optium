546e20c8e20d07fd16671927bbfd4a9d886b979f
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-05-10 12:01:47 +0200

    Add documentation

--- lib/optium.ex
+++ lib/optium.ex
@@ -1,6 +1,61 @@
 defmodule Optium do
   @moduledoc """
   Functions for validating optional arguments passed to your functions
+
+  To validate options with Optium, you need to provide it a option schema.
+  Schema is a map or a keyword list describing how each option should be
+  validated.
+
+  ## Examples
+
+      %{port: [required: true],
+        address: [required: true, default: {0, 0, 0, 0}]}
+
+  Based on the schema above, Optium knows that `:port` option is required,
+  and it  will return an `Optium.OptionMissingError` if it is not provided.
+  `:address` is also required, but by defining a default value you make sure
+  that no errors will be returned and that default value will be appended
+  to parsed options list instead. Use `Optium.parse/2` to parse and validate
+  options:
+
+      iex> schema = %{port:    [required: true],
+      ...>            address: [required: true, default: {0, 0, 0, 0}]}
+      iex> [port: 12_345, address: {127, 0, 0, 1}] |> Optium.parse(schema)
+      {:ok, [port: 12_345, address: {127, 0, 0, 1}]}
+      iex> [port: 12_345] |> Optium.parse(schema)
+      {:ok, [address: {0, 0, 0, 0}, port: 12_345]}
+      iex> [address: {127, 0, 0, 1}] |> Optium.parse(schema)
+      {:error, %Optium.OptionMissingError{key: :port}}
+
+  There is also `Optium.parse!/2`, which follows Elixir's convention of "bang
+  functions" and raises and error instead of returning it.
+
+      iex> schema = %{port:    [required: true],
+      ...>            address: [required: true, default: {0, 0, 0, 0}]}
+      iex> [address: {127, 0, 0, 1}] |> Optium.parse!(schema)
+      ** (Optium.OptionMissingError) option :port is required
+
+  ## Schema
+
+  Schemas are keyword lists or maps with atoms as keys, and validation
+  options lists as values. Currently two validation options are supported:
+  `:required` and `:default`. When option is `:required`, `parse/2` returns
+  `Optium.OptionMissingError` if it is not present in the provided options list.
+  This can be overriden by providing `:default` value, which is put into
+  provided options list if no such option is present.
+
+  Note that returned, validated options list contains only those options which
+  keys are present in the schema. You can add an option to schema with empty
+  validation options list (like `%{key: []}`), but it doesn't make much sense
+  because you most likely want to have some default value anyway.
+
+  ## Error handling
+
+  As mentioned before, `parse/2` returns `{:error, error}` tuple, where `error`
+  is a relevant exception struct. All Optium exceptions implement
+  `Exception.message/1` callback, so you can use `Exception.message(error)`
+  to provide nice error message to users of your function. `parse!/2` intercepts
+  exception structs and raises them instead of returning.
   """
 
   alias Optium.Metadata
@@ -14,6 +69,8 @@ defmodule Optium do
 
   @doc """
   Parses, validates and normalizes options based on passed Optium schema
+
+  See documentation for `Optium` module for more info.
   """
   @spec parse(opts, schema) :: {:ok, opts} | {:error, error}
   def parse(opts, schema) do

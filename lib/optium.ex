5f3ca31c9806dfebbe2176c71cc0c1d319df07d0
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-05-10 11:09:03 +0200

    Return only options defined in schema

--- lib/optium.ex
+++ lib/optium.ex
@@ -2,4 +2,62 @@ defmodule Optium do
   @moduledoc """
   Functions for validating optional arguments passed to your functions
   """
+
+  alias Optium.Metadata
+
+  @type key :: atom
+  @type opts :: Keyword.t
+  @type schema :: %{key => key_opts} | [{key, key_opts}]
+  @type key_opts :: [key_opt]
+  @type key_opt :: [{:required, boolean}, {:default, term}]
+  @type error :: OptionMissingError.t
+
+  @doc """
+  Parses, validates and normalizes options based on passed Optium schema
+  """
+  @spec parse(opts, schema) :: {:ok, opts} | {:error, error}
+  def parse(opts, schema) do
+    metadata = Optium.Metadata.from_schema(schema)
+
+    parsed =
+      opts
+      |> take_defined_options(metadata)
+
+    {:ok, parsed}
+  end
+
+  @spec take_defined_options(opts, Metadata.t) :: opts
+  defp take_defined_options(opts, metadata) do
+    Enum.reduce(metadata.keys, [], fn key, acc ->
+      take_opt_if_present(key, opts, acc)
+    end)
+  end
+
+  @spec take_opt_if_present(key, opts, acc :: opts) :: acc :: opts
+  defp take_opt_if_present(key, opts, acc) do
+    case Keyword.fetch(opts, key) do
+      {:ok, value} ->
+        Keyword.put(acc, key, value)
+      _ ->
+        acc
+    end
+  end
+
+  defmodule OptionMissingError do
+    @moduledoc """
+    Raised when option marked as `:required` is missing from the options list
+    """
+
+    defexception [:key]
+
+    @type t :: %__MODULE__{key: Optium.key}
+
+    def exception(key) do
+      %__MODULE__{key: key}
+    end
+
+    def message(struct) do
+      "option #{inspect struct.key} is required"
+    end
+  end
 end
--- test/optium_test.exs
+++ test/optium_test.exs
@@ -1,4 +1,20 @@
 defmodule OptiumTest do
   use ExUnit.Case
   doctest Optium
+
+  describe "parse/2" do
+    test "returns only options defined in schema" do
+      schema = %{key1: [],
+                 key2: [required: true]}
+      opts = [key1: 1, key2: 2, key3: 3]
+      keys = Map.keys(schema)
+
+      {:ok, parsed} = Optium.parse(opts, schema)
+
+      assert length(parsed) == length(keys)
+      assert Keyword.fetch(parsed, :key1) == {:ok, 1}
+      assert Keyword.fetch(parsed, :key2) == {:ok, 2}
+      assert Keyword.fetch(parsed, :key3) == :error
+    end
+  end
 end
